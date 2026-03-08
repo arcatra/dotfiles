a11baae91426c12aadc6db57bae55042be45d338
Deepak <deep.vemu.cse@gmail.com>
2026-03-08 20:19:33 +0530

    Added formatter: stevearc

--- .config/nvim/lua/arcatra/plugins/telescope.lua
+++ .config/nvim/lua/arcatra/plugins/telescope.lua
@@ -1,5 +1,6 @@
 return {
-    'nvim-telescope/telescope.nvim', tag = 'v0.2.1',
+    'nvim-telescope/telescope.nvim',
+    tag = 'v0.2.1',
     dependencies = {
         'nvim-lua/plenary.nvim',
         config = function()
@@ -8,7 +9,6 @@ return {
             vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
             vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
             vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
-
         end
 
     }
